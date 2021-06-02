ed2d4f1160e89a367b4475bbb9bb2143734214e4
Dipokal HHJ <>
2021-06-02 04:06:27 +0000

    Update all function Add color detection

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 # TextToImage 
 # DipokalHHJ
 
-import core
+from core import T2i
 
-pid = core.generatePrimaryId()
-print(pid)
-core.createEncryptionImage(core.createHex(pid))
+def main():
+  CRH = T2i()
+  pid_org = CRH.generatePrimaryId()
+  pid_hex = CRH.createHex(pid_org)
+
+  CRH.createEncryptionImage(pid_hex)
+
+  hex_color = CRH.getHexFromImage('result')
+  # hex_data = CRH.createRgbtoHex(rgb_color)
+
+  print(pid_org)
+  print(hex_color)
+  # print(hex_data)
+     
+if __name__ == "__main__":
+  main()
